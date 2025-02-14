6ac2b08635dccee94d3a1ee2bfa23c0dd49e4a73
Chaithanya <chaithanyainaganti.com>
2025-02-14 11:09:39 +0530

    Updated CORS settings for Vercel frontend

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://job-sphere-mu.vercel.app/',
     credentials: true
 }));
 
