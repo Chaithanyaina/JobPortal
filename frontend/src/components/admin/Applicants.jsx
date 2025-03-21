81bd1f2b6509b8e57fe646823faab03c63851bb7
Chaithanya <chaithanyainaganti.com>
2025-03-21 16:26:46 +0530

    latest

--- frontend/src/components/admin/Applicants.jsx
+++ frontend/src/components/admin/Applicants.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react'
-import Navbar from '../shared/Navbar'
-import ApplicantsTable from './ApplicantsTable'
+import React, { useEffect } from 'react';
+import Navbar from '../shared/Navbar';
+import ApplicantsTable from './ApplicantsTable';
 import axios from 'axios';
 import { APPLICATION_API_END_POINT } from '@/utils/constant';
 import { useParams } from 'react-router-dom';
@@ -10,28 +10,33 @@ import { setAllApplicants } from '@/redux/applicationSlice';
 const Applicants = () => {
     const params = useParams();
     const dispatch = useDispatch();
-    const {applicants} = useSelector(store=>store.application);
+    const { applicants } = useSelector(store => store.application);
 
     useEffect(() => {
+        if (!params.id) return;
+
         const fetchAllApplicants = async () => {
             try {
                 const res = await axios.get(`${APPLICATION_API_END_POINT}/${params.id}/applicants`, { withCredentials: true });
                 dispatch(setAllApplicants(res.data.job));
             } catch (error) {
-                console.log(error);
+                console.error("Error fetching applicants:", error);
             }
-        }
+        };
         fetchAllApplicants();
-    }, []);
+    }, [params.id, dispatch]);
+
     return (
         <div>
             <Navbar />
             <div className='max-w-7xl mx-auto'>
-                <h1 className='font-bold text-xl my-5'>Applicants {applicants?.applications?.length}</h1>
+                <h1 className='font-bold text-xl my-5'>
+                    Applicants {applicants?.applications ? applicants.applications.length : 0}
+                </h1>
                 <ApplicantsTable />
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Applicants
+export default Applicants;
--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,9 +1,8 @@
-const isLocal = window.location.hostname === "localhost"; // Check if running locally
 
 const LOCAL_BASE_URL = "http://localhost:8000";
 const ENV_BASE_URL = import.meta.env.VITE_API_URL; // VITE_API_URL from .env for production
 
-const BASE_URL = isLocal ? LOCAL_BASE_URL : ENV_BASE_URL; // Auto-switch based on environment
+const BASE_URL = LOCAL_BASE_URL || ENV_BASE_URL
 
 export const USER_API_END_POINT = `${BASE_URL}/api/v1/user`;
 export const JOB_API_END_POINT = `${BASE_URL}/api/v1/job`;
