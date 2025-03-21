af1f6e12e8870d51a799314b674d2123407e641d
Chaithanya <chaithanyainaganti.com>
2025-03-21 15:36:23 +0530

    New

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -131,10 +131,10 @@ export const login = async (req, res) => {
       return res
         .status(200)
         .cookie("token", token, {
-          maxAge: 1 * 24 * 60 * 60 * 1000, // 1 day
+          maxAge: 1 * 24 * 60 * 60 * 1000,
           httpOnly: true,
-          sameSite: 'strict',
-          secure: process.env.NODE_ENV === 'production', // Enable secure cookies in production
+          secure: process.env.NODE_ENV === "production", // HTTPS only in production
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // Allow cross-site cookies
         })
         .json({
           message: `Welcome back ${user.fullname}`,
